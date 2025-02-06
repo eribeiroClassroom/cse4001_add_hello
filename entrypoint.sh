f19f06ad2caf2fe5d3f7cd1c32034e5fddae026a
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-02-06 18:21:51 -0500

    Using build_os161 script

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,31 +15,41 @@ echo "Updated PATH variable in the container"
 export PATH=$PATH:/root/os161/tools/bin
 echo $PATH
 
-echo "Configuring OS/161"
-cd kern/conf
-./config ASST3
-
-echo "Building OS/161 (Kernel-level)"
-cd ../compile/ASST3
-/root/os161/tools/bin/bmake depend
-/root/os161/tools/bin/bmake
-
 # Set (or re-set) the HOME variable otherwise 
 # GitHub will set it to HOME = "/github/home/"
 export HOME="/root"
 
-/root/os161/tools/bin/bmake install
 
-echo "List of files in the directory"
-ls -l 
 
-# Build OS/161 (Userland)
-cd /github/workspace
-/root/os161/tools/bin/bmake includes
-cd /github/workspace
-/root/os161/tools/bin/bmake
-cd /github/workspace
-/root/os161/tools/bin/bmake install
+echo "Building OS/161"
+./build_os161
+
+
+# echo "Configuring OS/161"
+# cd kern/conf
+# ./config ASST3
+
+# echo "Building OS/161 (Kernel-level)"
+# cd ../compile/ASST3
+# /root/os161/tools/bin/bmake depend
+# /root/os161/tools/bin/bmake
+
+# # Set (or re-set) the HOME variable otherwise 
+# # GitHub will set it to HOME = "/github/home/"
+# export HOME="/root"
+
+# /root/os161/tools/bin/bmake install
+
+# echo "List of files in the directory"
+# ls -l 
+
+# # Build OS/161 (Userland)
+# cd /github/workspace
+# /root/os161/tools/bin/bmake includes
+# cd /github/workspace
+# /root/os161/tools/bin/bmake
+# cd /github/workspace
+# /root/os161/tools/bin/bmake install
 
 # Run the OS/161 kernel with the hello program
 ./expect_run_hello.exp 
