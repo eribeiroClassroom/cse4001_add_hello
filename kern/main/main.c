b8aace83f1a94b212e132ac367aeb475fa0583bb
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2025-02-06 21:57:21 -0500

    test

--- kern/main/main.c
+++ kern/main/main.c
@@ -101,7 +101,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Hello CSE4001 system version %s (%s #%d)\n",
+	kprintf("Put-your-group-name-here's system version %s (%s #%d)\n",
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
